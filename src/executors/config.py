562278254bbf7ced3ad0720a8e1c706756937dcd
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-07-18 12:35:26 +0300

    Add comment.

--- src/executors/config.py
+++ src/executors/config.py
@@ -8,6 +8,8 @@ if  \
     or  CONFIG not in globals()\
     or  CONFIG not in locals()\
 :
+    # If module "config" not imported, not defined in environments
+    # create instance of dummy class "Config"
     class Config:
         DEBUG = True\
                     if hasattr(sys, "gettrace") and sys.gettrace()\
