7d728a430baf51e827eb06459909c5d3750c7bc3
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-08-08 20:12:04 +0300

    Moved common code to common.py.

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -6,6 +6,7 @@ if "logger.config" in sys.modules:
         or  __name__ == "test_executors"    \
         :
         config.config.SCRIPT = __file__
+        config.config.LOG_FILE_TRUNCATE = True
         Logging.truncate()
         Logging.init()
 else:
@@ -16,129 +17,131 @@ else:
 
 
 class MainThread(CommonTestCase):
+    # name = "mainthread"
 
     def test(self):
-        name = "mainthread"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        submit_tasks(executor, TIMEOUT)
-        results = get_results(executor.results)
-
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
-
+        self._test()
 
 
 class Thread(CommonTestCase):
 
     def test(self):
-        name = "thread"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
-        # actives = active_threads(monitoring)
-        results = get_results(executor.results)
+        self._test()
+    # def test(self):
+    #     name = "thread"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
+    #     # actives = active_threads(monitoring)
+    #     results = get_results(executor.results)
 
-        # self.assertEqual(executor.iworkers.value, 1)
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
+    #     # self.assertEqual(executor.iworkers.value, 1)
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
 
 
 
 class Process(CommonTestCase):
+    pass
+    # def test(self):
+    #     name = "process"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
 
-    def test(self):
-        name = "process"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
+    #     # actives = active_processes(monitoring)
+    #     results = get_results(executor.results)
 
-        # actives = active_processes(monitoring)
-        results = get_results(executor.results)
-
-        # self.assertEqual(actives, 1)
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
+    #     # self.assertEqual(actives, 1)
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
 
 
 
 class Threads(CommonTestCase):
 
     def test(self):
-        name = "threads"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
-        # actives = active_threads(monitoring)
-        results = get_results(executor.results)
+        self._test()
+    # def test(self):
+    #     name = "threads"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
+    #     # actives = active_threads(monitoring)
+    #     results = get_results(executor.results)
 
-        # self.assertEqual(actives, os.cpu_count())
-        # self.assertEqual(executor.iworkers.value, os.cpu_count())
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
+    #     # self.assertEqual(actives, os.cpu_count())
+    #     # self.assertEqual(executor.iworkers.value, os.cpu_count())
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
 
 
 
 class Processes(CommonTestCase):
 
     def test(self):
-        name = "processes"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
-        # actives = active_processes(monitoring)
-        results = get_results(executor.results)
+        self._test()
+    # def test(self):
+    #     name = "processes"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
+    #     # actives = active_processes(monitoring)
+    #     results = get_results(executor.results)
 
-        # self.assertEqual(actives, os.cpu_count())
-        # self.assertEqual(executor.iworkers.value, os.cpu_count())
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
-        return
+    #     # self.assertEqual(actives, os.cpu_count())
+    #     # self.assertEqual(executor.iworkers.value, os.cpu_count())
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
+    #     return
 
 
 
 class ThreadPool(CommonTestCase):
 
     def test(self):
-        name = "threadpool"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
-        # actives = active_threads(monitoring)
-        results = get_results(executor.results)
+        self._test()
+    # def test(self):
+    #     name = "threadpool"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
+    #     # actives = active_threads(monitoring)
+    #     results = get_results(executor.results)
 
-        # self.assertEqual(actives, os.cpu_count())
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
+    #     # self.assertEqual(actives, os.cpu_count())
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
 
 
 
 class ProcessPool(CommonTestCase):
 
     def test(self):
-        name = "processpool"
-        logger.info(f"Testing '{name}' start.")
-        executor = registry[name]()
-        monitoring = create_monitoring(executor, TIMEOUT)
-        monitoring.join()
-        # actives = active_processes(monitoring)
-        results = get_results(executor.results)
+        self._test()
+    # def test(self):
+    #     name = "processpool"
+    #     logger.info(f"Testing '{name}' start.")
+    #     executor = registry[name]()
+    #     monitoring = create_monitoring(executor, TIMEOUT)
+    #     monitoring.join()
+    #     # actives = active_processes(monitoring)
+    #     results = get_results(executor.results)
 
-        # self.assertEqual(actives, os.cpu_count())
-        self.assertEqual(len(results), TASKS)
-        self.assertEqual(sorted(self.results), sorted(results))
-        logger.info(f"Testing '{name}' end.")
+    #     # self.assertEqual(actives, os.cpu_count())
+    #     self.assertEqual(len(results), TASKS)
+    #     self.assertEqual(sorted(self.results), sorted(results))
+    #     logger.info(f"Testing '{name}' end.")
 
 
 
