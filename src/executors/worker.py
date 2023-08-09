923518d29a08f2e9e99fb8b11b6a3a720865c00d
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-08-09 12:21:21 +0300

    Add joining to native Thread or Process.
    Move task getter to Executor.

--- src/executors/worker.py
+++ src/executors/worker.py
@@ -8,7 +8,7 @@ from executors import Logging
 from executors import descriptors
 
 from executors.config   import CONFIG
-from executors.executor import Executor
+from executors.executor import Executor, TASK_SENTINEL, RESULT_SENTINEL
 from executors.logger   import logger
 
 
@@ -61,7 +61,7 @@ class InProcess(descriptors.InChilds):
 
 class Worker(Executor):
     
-    MAX_TRIES = 0
+    MAX_TRIES = 1
     MAX_UNITS = 1
 
     # executor_creation   = ExecutorCreationAllowed
@@ -74,6 +74,45 @@ class Worker(Executor):
         ):
         super(Worker, self).__init__(parent_pid, parent_tid)
 
+    def join(self, timeout = None) -> bool:
+        #super(Worker, self).join(timeout)
+        if      not self.in_executor                \
+            and self.executor is not None           \
+        :
+            tries = 0
+            while tries < Worker.MAX_TRIES:
+                info = ""
+                info += f"{Logging.info(self.__class__.__name__)}. "
+                info += f"Going to wait for {self.executor}" 
+                info += f" for {timeout}sec" if timeout else "."
+                logger.debug(info)
+
+                # if timeout is None:
+                #     self.joined.value = 1
+
+                self.executor.join(timeout)
+                if self.executor.is_alive():
+                    logger.debug(
+                        f"{Logging.info(self.__class__.__name__)}. "
+                        f"{self.executor} not complete in {timeout}sec."
+                    )
+                    if timeout is not None:
+                        tries += 1
+                    else:
+                        return False
+                else:
+                    self.joined.value = 1
+                    return True
+            else:
+                if timeout is not None and tries >= 3:
+                    logger.debug(
+                        f"{Logging.info(self.__class__.__name__)}. "
+                        f"{self.executor} not complete with "
+                        f"{timeout*tries}sec. "
+                        f"Try count reached {tries}. Exiting."
+                    )
+        return False
+    
     @staticmethod
     def worker(executor: Executor, conf = None, /, *args, **kwargs):
         caller = executor.__class__.__name__
@@ -97,7 +136,7 @@ class Worker(Executor):
             logger.debug(f"{Logging.info(caller)} logging prepeared.")
 
         logger.debug(f"{Logging.info(caller)} started.")
-        # executor.started = True
+        executor.started = True
         if conf is not None and hasattr(conf, "DEBUG") and conf.DEBUG:
             processed = 0
         while True:
@@ -105,43 +144,22 @@ class Worker(Executor):
             logger.debug(
                 f"{Logging.info(caller)}. "
                 "<Status "
+                    f"workers={executor.iworkers.value} "
                     f"tasks={executor.tasks.qsize()} "
                     f"results={executor.results.qsize()}"
                 ">."
             )
-            task = executor.tasks.get()  # Get task or wait for new one
+            task = executor.get_task()
 
-            # Filter out all sentinel
-            # and push back one on empty tasks' queue
-            if task is None:
-                if  isinstance(
-                        executor.tasks,
-                        multiprocessing.queues.JoinableQueue
-                    ):
-                    executor.tasks.task_done()
-                # Get and mark all sentinel tasks as done
-                tries = 0
-                while task is None:
-                    try:
-                        task = executor.tasks.get_nowait()
-                        if task is None:
-                            if  isinstance(
-                                    executor.tasks,
-                                    multiprocessing.queues.JoinableQueue
-                                ):
-                                executor.tasks.task_done()
-                        else:
-                            break
-                    except Exception as e: # Fucking shit
-                        task = None
-                        if tries < Worker.MAX_TRIES:
-                            tries += 1
-                            time.sleep(tries)
-                            continue
-                        # On empty put back sentinel for other processes
-                        if executor.iworkers.value > 1:
-                            executor.tasks.put_nowait(None)
-                        break
+            # On empty put back sentinel for other workers
+            if task is None and executor.iworkers.value > 0:
+                executor.tasks.put_nowait(TASK_SENTINEL)
+                logger.info(
+                    f"{Logging.info(caller)}. "
+                    f"{task} - sentinel putted back."
+                )
+            elif task is None:
+                pass
             
             if task is not None:
                 # # TODO: Move out.
@@ -179,8 +197,8 @@ class Worker(Executor):
                     ):
                     executor.tasks.task_done()
                 # Process results
-                if result and result != executor.results:
-                    executor.process_results(result)
+                # if result and result != executor.results:
+                Executor.process_results(executor.results, result)
             else:
                 logger.debug(
                     f"{Logging.info(caller)} "
@@ -192,15 +210,24 @@ class Worker(Executor):
         logger.debug(
             f"{Logging.info(caller)}. "
             "<Status "
+                f"workers={executor.iworkers.value} "
                 f"tasks={executor.tasks.qsize()} "
                 f"results={executor.results.qsize()}"
             ">."
         )
-        # executor.iworkers.value -= 1
-        # executor.shutdown()
         executor.is_shutdown.value = 1
-        executor.iworkers.value -= 1
         if executor.create is not None:
             logger.debug(f"{Logging.info(caller)} exiting signaled. ")
             executor.create.set()
+
+        executor.shutdown(False)
+        executor.iworkers.value -= 1
+        # executor.results.put_nowait(RESULT_SENTINEL)
         return
+
+    # def shutdown(self, wait = True, * , cancel = False) -> bool:
+    #     if super().shutdown(wait, cancel=cancel):
+    #         if self.executor is not None:
+    #             self.executor.close()
+    #             return True
+    #     return False
